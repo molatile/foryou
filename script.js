6cc3c485fc8a1f1b7a40d229cbd013a76205df62
molatile <gobhi666666@gmail.com>
2026-09-18 18:08:06 +0530

    custom

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ let write=async()=>{
 };
 write();
 
-const img=["s1","s2","s3","s4","s5","s6","s7","s8","s9","s10","s11","s12","s13"];
+const img=["s1","s2","s3","s4","s5"];
 let i=0;
 let slide=document.querySelector(".slide");
 
